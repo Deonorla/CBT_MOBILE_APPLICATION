379c3c628fe0336915aa2c274f70115d4adbeee1
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-06-22 01:52:58 +0100

    [firebase cli initialization completed]

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+// Import the generated file
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
