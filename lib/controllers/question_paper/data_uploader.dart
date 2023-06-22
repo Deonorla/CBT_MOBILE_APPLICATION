3028728797edee53b9d40456cf34f578c561c9ed
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-06-22 11:14:33 +0100

    [models setup completed]

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:cbt_mobile_application/models/question_paper_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -16,14 +17,18 @@ class DataUploader extends GetxController {
     final manifestContent = await DefaultAssetBundle.of(Get.context!)
         .loadString("AssetManifest.json");
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
-    // load json files and print files
+    // load json files and print path
     final papersInAssets = manifestMap.keys
         .where((path) =>
             path.startsWith("assets/DB/papers") && path.contains(".json"))
         .toList();
+    List<QuestionPaperModel> questionPapers = [];
     for (var paper in papersInAssets) {
       String stringPaperContent = await rootBundle.loadString(paper);
-      print(stringPaperContent);
+      // json.decode takes a string and convert it to a map
+      questionPapers
+          .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
     }
+    print('Items number ${questionPapers.length}');
   }
 }
